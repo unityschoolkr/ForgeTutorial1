06016405608fc84aceaf400aecba70909d91f317
unityschookr <unityschool@naver.com>
2020-12-20 09:46:31 +0900

    Network Instantiation

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BeardedManStudios.Forge.Networking.Unity;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        NetworkManager.Instance.InstantiatePlayerCube();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
